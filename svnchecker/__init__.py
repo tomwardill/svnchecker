2ffd86f7a684b428af9005588325a844bdbc37a8
Tom Wardill <tom@howrandom.net>
2011-06-24 15:35:42 +0100

    Can execute tests based on buildout parameters

--- svnchecker/__init__.py
+++ svnchecker/__init__.py
@@ -1,3 +1,17 @@
+import os
+from subprocess import call
 
-def main():
-    print "foo" * 100
+
+def main(args):
+    print args
+
+    # execute the tests
+    command_path = os.path.join(args['bin'], args['command'])
+    command = command_path + ' test '
+    command += ' '.join([app + ' ' for app in args['apps']])
+    result = call(command, shell = True)
+
+    if result == 0:
+        print "pass"
+    else:
+        print "fail"
