9ad3e901643ca0ea9a82c7ae1a773d1ae12c2531
Tom Wardill <tom@howrandom.net>
2011-06-24 15:44:02 +0100

    Working, first version

--- svnchecker/__init__.py
+++ svnchecker/__init__.py
@@ -1,17 +1,25 @@
 import os
 from subprocess import call
-
+from time import sleep
+import sys
 
 def main(args):
-    print args
 
-    # execute the tests
-    command_path = os.path.join(args['bin'], args['command'])
-    command = command_path + ' test '
-    command += ' '.join([app + ' ' for app in args['apps']])
-    result = call(command, shell = True)
+    if sys.argv[1] == 'commit' or sys.argv[1] == 'ci':
+        print "Waiting 10 seconds in case of accidental checkin oops"
+        sleep(10)
 
-    if result == 0:
-        print "pass"
+        # execute the tests
+        command_path = os.path.join(args['bin'], args['command'])
+        command = command_path + ' test '
+        command += ' '.join([app + ' ' for app in args['apps']])
+        result = call(command, shell = True)
+
+        if result == 0:
+            # now we can check in
+            print "Tests passed, commencing checkin"
+            call(['svn'] + sys.argv[1:])
+        else:
+            print "Your tests have failed, not checking in"
     else:
-        print "fail"
+        call(['svn'] + sys.argv[1:])
